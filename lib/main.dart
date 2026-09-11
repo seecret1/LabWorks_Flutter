7cad3476bb2fefd0872a148cb8d44da06b9c6a7e
seecret1 <naintonight@gmail.com>
2026-09-11 22:03:34 +0300

    feat: lab 3 created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:labwork1/service/orderService.dart';
-
-import 'entity/order.dart';
-import 'entity/orderStatus.dart';
-import 'entity/user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,37 +31,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  int _counter = 0;
-
-  String? _lastOrderUser;
-
-  OrderService _orderService = new OrderService();
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  Future<void> _createOrder() async {
-    final randomName = "User: $_counter";
-    final user = User(randomName);
-
-    Order order = await _orderService.createOrder(
-      user,
-      comment: 'Тестовый заказ $randomName',
-    );
-    String orderName = order.getName;
-    OrderStatus status = order.getStatus;
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Заказ: $orderName для $randomName получил статус: $status'),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,41 +38,133 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      _CardData(
+        'Chevrolet',
+        descriptionText: "Chevrolet Camaro, Color: white",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnz7GS5qj950_p6To68O1ohYkgz_grCZiqr9gdmuh5aWuE1oqu51ke-VE&s=10"
+      ),
+      _CardData(
+        'Lamborghini',
+        icon: Icons.eleven_mp,
+        descriptionText: "Lamborghini, Color: blue",
+        imageUrl: "https://robbreport.com/wp-content/uploads/2024/08/temerario01.jpg?w=800"
+      ),
+      _CardData(
+        'Chevrolet',
+        icon: Icons.sixteen_mp,
+        descriptionText: "Chevrolet Corvette, Color: red",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUv-Tr_Bu2mFGuGiULhuHJPOXVqK_Uj5Kh1KI6EpqFtBQMAmnaKEFsvVj8&s=10"
+      ),
+    ];
+    return Center(
+      child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-              onPressed: _createOrder,
-              icon: const Icon(Icons.add_shopping_cart),
-              label: const Text('Создать заказ'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            if (_lastOrderUser != null)
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Text(
-                  'Последний заказ: $_lastOrderUser',
-                  style: const TextStyle(fontSize: 14, color: Colors.grey),
+          children: data.map((e) => _Card.fromData(e)).toList(),
+        ),
+      )
+    );
+  }
+}
+
+class _CardData {
+  final String text;
+  final IconData icon;
+  final String descriptionText;
+  final String? imageUrl;
+
+  const _CardData(
+      this.text, {
+        this.icon = Icons.eighteen_mp_sharp,
+        required this.descriptionText,
+        this.imageUrl
+      });
+
+}
+
+class _Card extends StatelessWidget {
+
+  final String text;
+  final IconData icon;
+  final String descriptionText;
+  final String? imageUrl;
+
+  const _Card(
+      this.text, {
+        this.icon = Icons.ac_unit_outlined,
+        required this.descriptionText,
+        this.imageUrl
+  });
+
+  factory _Card.fromData(_CardData data) => _Card(
+    data.text,
+    descriptionText: data.descriptionText,
+    icon: data.icon,
+    imageUrl: data.imageUrl
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.black,
+          width: 5
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+              child: SizedBox(
+                height: 150,
+                width: 100,
+                child: Image.network(
+                  imageUrl ?? '',
+                  fit: BoxFit.cover,
+                  errorBuilder: (_, __, ___) => const Placeholder(),
                 ),
               ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      text,
+                    style: Theme.of(context).textTheme.headlineLarge
+                  ),
+                  Text(
+                      descriptionText,
+                      style: Theme.of(context).textTheme.headlineSmall
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(icon),
+          )
+        ],
       ),
     );
   }
